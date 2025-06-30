2347724c794a51fb97633d094d85393de1dd5dfa
Jakob Huuse <jakob.huuse@gmail.com>
2025-06-30 13:42:07 +0200

    Use typescript

--- backend/app.ts
+++ backend/app.ts
@@ -1,3 +1,5 @@
+import { Request, Response, NextFunction} from "express";
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -23,18 +25,18 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req: Request, res: Response, next: NextFunction) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err: Error, req: Request, res: Response, next : NextFunction) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  res.status(500);
   res.render('error');
 });
 
